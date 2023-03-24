7ffb4ba5ad230cd9da43ae0773102a9703787bc7
jjb-dev <84111581+jjbuell-dev@users.noreply.github.com>
2023-03-24 11:49:57 -0600

    Initial Project Setup

---
+++ Weather-MVVM/ViewControllers/RootViewController/RootViewController.swift
@@ -0,0 +1,22 @@
+//
+//
+// Weather-MVVM
+// 
+// RootViewController.swift
+//
+// Created by Royal Blue Software
+// 
+
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemIndigo
+    }
+}
